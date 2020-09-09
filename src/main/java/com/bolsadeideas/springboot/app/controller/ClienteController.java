332c8da61f20642b730d2036e40cb12cb01e9dae
yoelenriquez <yoel.enriquez.1995@gmail.com>
2020-09-08 23:19:26 -0300

    Implementacion de la capa de servicio para cliente

--- src/main/java/com/bolsadeideas/springboot/app/controller/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controller/ClienteController.java
@@ -16,18 +16,19 @@ import org.springframework.web.bind.support.SessionStatus;
 
 import com.bolsadeideas.springboot.app.models.dao.IClienteDao;
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
+import com.bolsadeideas.springboot.app.models.service.IClienteService;
 
 @Controller
 @SessionAttributes("cliente")
 public class ClienteController {
 	
 	@Autowired
-	private IClienteDao clienteDao;
+	private IClienteService clienteService;
 	
 	@RequestMapping(value = "/listar", method = RequestMethod.GET)
 	public String listar(Model model) {
 		model.addAttribute("titulo", "Listado de cliente");
-		model.addAttribute("clientes", clienteDao.findAll());
+		model.addAttribute("clientes", clienteService.findAll());
 		
 		return "listar";
 	}
@@ -46,7 +47,7 @@ public class ClienteController {
 		Cliente cliente = null;
 		
 		if(id > 0) {
-			cliente = clienteDao.findOne(id);
+			cliente = clienteService.findOne(id);
 		} else {
 			return "redirect:/listar";
 		}
@@ -67,7 +68,7 @@ public class ClienteController {
 			return "form";
 		}
 		
-		clienteDao.save(cliente);
+		clienteService.save(cliente);
 		status.setComplete();
 		return "redirect:listar";
 	}
@@ -75,7 +76,7 @@ public class ClienteController {
 	@RequestMapping(value = "/eliminar/{id}")
 	public String eliminar(@PathVariable(value = "id") Long id) {
 		if(id > 0) {
-			clienteDao.eliminar(id);
+			clienteService.eliminar(id);
 		}
 		
 		return "redirect:/listar";
--- src/main/java/com/bolsadeideas/springboot/app/models/dao/ClienteDaoImple.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/dao/ClienteDaoImple.java
@@ -6,7 +6,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
 
@@ -17,7 +16,7 @@ public class ClienteDaoImple implements IClienteDao {
 	private EntityManager em;
 
 	@SuppressWarnings("unchecked")
-	@Transactional(readOnly = true)
+	
 	@Override
 	public List<Cliente> findAll() {
 		// TODO Auto-generated method stub
@@ -25,7 +24,6 @@ public class ClienteDaoImple implements IClienteDao {
 	}
 
 	@Override
-	@Transactional
 	public void save(Cliente cliente) {
 		if(cliente.getId() != null && cliente.getId() > 0) {
 			//actualiza
@@ -37,13 +35,11 @@ public class ClienteDaoImple implements IClienteDao {
 	}
 
 	@Override
-	@Transactional(readOnly = true)
 	public Cliente findOne(Long id) {
 		return em.find(Cliente.class, id);
 	}
 
 	@Override
-	@Transactional
 	public void eliminar(Long id) {
 		Cliente cliente = findOne(id);
 		em.remove(cliente);
